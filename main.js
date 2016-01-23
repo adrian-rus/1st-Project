4b45925e05cf36bebcdbb71ceb19c54be4328147
Adi <adrian.rus@live.com>
2016-01-23 17:23:29 +0000

    add hidden search bar on navigation

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
-$(document).ready(function(){ //changed this because I had a feeling the other statement did not load the script
+$(document).ready(function(){ 
     
-   /*$("btn").on({
-        .mouseenter: function(){
-        $(this).removeClass("btn-danger").addClass("btn-default")
-        },
-    
-        .mouseleave:function(){
-        $(this).addClass("btn-danger").removeClass("btn-default");
-        }
-   }); //nothing happends*/
-    
-    $("btn").click(function(){
-        $(".lnd").hide(); //img does not hide
-    });
-    
-   $("#add").click(function(){
-       $('.txt').text('added text'); //no text added
+    $('#search-btn').click(function(){
+      $('#search-bar').show();
    });
-   
-   $(".i").click(function(){
-       $(this).css('opacity',0.85); //this seems the only one that works... can't figure out why the other ones aren't doing anything...
-   })
-   
-   $(".name").click(function(){
-       $(this).show("preview");
-   })
 });
